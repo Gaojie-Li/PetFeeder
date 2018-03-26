b02756b589e48015b51730f6947157d0d791ff64
Gaojie-Li <i.am.hya.li@gmail.com>
2018-03-26 00:52:14 -0400

    Add main story board and corresponding view controller (but empty = =)

---
+++ PetFeeder/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  PetFeeder
+//
+//  Created by Gaojie Li on 3/26/18.
+//  Copyright © 2018 Gaojie Li. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
