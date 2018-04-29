db7759bb792fae139c6ad1b9b0320ab1687c5dab
Gaojie-Li <i.am.hya.li@gmail.com>
2018-04-29 16:59:30 -0400

    Add side bar in main view, add profile page and pet profile page but haven't finished yet

--- PetFeeder/MainViewController.swift
+++ PetFeeder/MainViewController.swift
@@ -10,6 +10,32 @@ import Foundation
 import UIKit
 
 class MainViewController : UIViewController {
+    
+    @IBOutlet weak var trailingConstraints: NSLayoutConstraint!
+    @IBOutlet weak var leadingConstraints: NSLayoutConstraint!
+    @IBOutlet weak var mainView: UIView!
+    var hamburgerMenuVisible = false
+    
+    @IBAction func hamburgerTapped(_ sender: Any) {
+        if !hamburgerMenuVisible {
+            leadingConstraints.constant = 150
+            trailingConstraints.constant = 150
+            
+            hamburgerMenuVisible = true
+        } else {
+            leadingConstraints.constant = 0
+            trailingConstraints.constant = 0
+            
+            hamburgerMenuVisible = false
+        }
+        
+        UIView.animate(withDuration: 0.2, delay: 0.0, options: .curveEaseIn, animations: {
+            self.view.layoutIfNeeded()
+        }) { (animationComplete) in
+            print("The animation is complete.")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
